4f95ebc2fe2f5c5a3a2af1480825bd1d86a67d07
soy <kksm456@naver.com>
2022-07-13 19:21:58 +0900

    first commit

---
+++ src/components/button/index.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export let text: string;
+    export let onClick: (value: string) => void;
+    export let color: "orange" |"gray" | "dark-gray";
+</script>
+
+<div class = "button" data-color = {color} on:click={()=> onClick(text) }>{text}</div>
+
+<style lang="scss">
+    .button{
+        width: 80px;
+        height: 80px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        &[data-color="orange"]{
+            background-color: orange;
+        }
+        &[data-color="gray"]{
+            background-color: gray;
+        }
+        &[data-color="dark-gray"]{
+            background-color:darkgray;
+        }
+    }
+</style>
