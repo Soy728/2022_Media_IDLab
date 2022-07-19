1780d4bf4a5a4ab3560fb894b17248b4be012b33
Soy728 <kksm456@naver.com>
2022-07-20 05:14:31 +0900

    부끄러운 계산기

--- src/components/button/index.svelte
+++ src/components/button/index.svelte
@@ -1,26 +1,32 @@
 <script lang="ts">
-    export let text: string;
-    export let onClick: (value: string) => void;
-    export let color: "orange" |"gray" | "dark-gray";
+  export let text: string;
+  export let onClick: (value: string) => void;
+  export let color: "orange" |"gray" | "dark-gray";
 </script>
 
-<div class = "button" data-color = {color} on:click={()=> onClick(text) }>{text}</div>
+<div class = "button" on:click={()=> onClick(text)} data-color = {color}>{text}</div>
 
 <style lang="scss">
-    .button{
-        width: 80px;
-        height: 80px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        &[data-color="orange"]{
-            background-color: orange;
-        }
-        &[data-color="gray"]{
-            background-color: gray;
-        }
-        &[data-color="dark-gray"]{
-            background-color:darkgray;
-        }
-    }
+  .button{
+      width: 80px;
+      height: 80px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 28px;
+      color: white;
+      border: 0.1px solid rgb(83, 83, 83);
+      &[data-color="orange"]{
+          background-color: orange;
+      }
+      &[data-color="gray"]{
+          background-color: gray;
+      }
+      &[data-color="dark-gray"]{
+          background-color:darkgray;
+      }
+  }
+
+
+
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,19 +1,82 @@
 <script lang="ts">
 	import { Button } from '@src/components/button';
 	//script area
-	const textNumbers: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-</script>
+	const textNumbers: string[] = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0','.'];
+	const texttop: string[] =["   C   "];
+	const text:string="C";
+	const textOperations: string[] = ['+','-','x','÷',"="];
+	let total:string = "";
+ 
+	function returnnum(num:string):void{
+	   if(num == "C"){
+		  total = "";
+	   }
+	   else if(num == "="){
+		  total = eval(total);
+	   }
+	   else if(num == "x"){
+		  total +="*"
+	   }
+	   else if(num == "÷"){
+		  total += "/"
+	   }
+	   else{
+		  total += num;
+	   }
+ 
+	}
+	
+ </script>
 
-<div class="frame">
-	{#each textNumbers as text}
-		<Button {text} onClick={(value) => alert(value)} color="orange" />
-	{/each}
+ <div class = calculator>
+ 	<div class="container">
+		<div class = "display">{total}</div>
+	</div>
+	<div class = "mainpad">
+ 		<div class="frame">
+			
+			<Button {text}onClick={(value) => returnnum(value)} color="gray" />
+			
+			<!-- <Button {texttop} onClick={(value) => returnnum(value)} color="gray" /> -->
+
+			{#each textNumbers as text}
+	  			<Button {text} onClick={(value) => returnnum(value)} color="dark-gray" />
+			{/each}
+		</div>
+ 	</div>
+     <div class="sidebar">   
+ 		<div class="operframe">
+			{#each textOperations as text}
+	   			<Button {text} onClick={(value) => returnnum(value)} color="orange" />
+			{/each}
+ 		</div>
+	</div>   
 </div>
 
 <style land="scss">
-	.frame {
-		width: 240px;
-		display: flex;
-		flex-wrap: wrap;
+	.container{
+		width: 328px;
+		height: 100px;
+		background-color: rgb(53, 53, 53);
+		position: relative;
 	}
-</style>
+
+	.display{
+		color: white;
+		font-size: 28px;
+		position: absolute;
+		top: 25px;
+		right: 12px;
+
+	}
+
+	.frame {
+	   width: 246px;
+	   display: flex;
+	   flex-wrap: wrap;
+	}
+	.mainpad {
+		float: left;
+		margin: 0;
+	   	}
+ </style>
