0320871218294ec01aaa0354443a9fc66ad64e69
Soy728 <kksm456@naver.com>
2022-07-27 05:20:16 +0900

    week 3

--- src/routes/bargraph/index.svelte
+++ src/routes/bargraph/index.svelte
@@ -1,14 +1,7 @@
 <script lang="ts">
 	import { Bar } from '@src/components/bar';
 	//script area
-	let data:number[] = [
-		20,
-		10,
-		15,
-		30,
-		5,
-		30
-	]
+	let data:number[] = [20,10,15,30,5,30]
 </script>
 
 <h1>Bar graph</h1>
